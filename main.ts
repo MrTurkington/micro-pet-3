36f0812f1b0fee1aaadc1083dce9e1463757fd6a
MrTurkington <97165847+MrTurkington@users.noreply.github.com>
2022-01-05 12:20:21 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,97 @@
+input.onButtonPressed(Button.A, function () {
+    loneliness = 0
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.FreeFall, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # . . . #
+        . . . . .
+        `)
+})
+let loneliness = 0
+music.playTone(349, music.beat(BeatFraction.Whole))
+music.playTone(294, music.beat(BeatFraction.Double))
+music.playTone(262, music.beat(BeatFraction.Breve))
+loneliness = 0
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # . . . #
+    . # # # .
+    . . . . .
+    `)
+basic.forever(function () {
+    basic.pause(1000)
+    loneliness += 1
+    if (loneliness < 15) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . # # # .
+            . . . . .
+            `)
+    } else if (loneliness < 40) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            # . . . #
+            . . . . .
+            `)
+        music.playTone(139, music.beat(BeatFraction.Whole))
+    }
+})
